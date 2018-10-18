26e75a75709480aa5400f749c3c9c74796316672
David Baker <dave@matrix.org>
2018-10-18 19:49:46 +0100

    Minimal PoC for https://github.com/electron/electron/issues/15251

--- renderer.js
+++ renderer.js
@@ -1,3 +1,44 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+function makeitcrash() {
+    let notif1open = true;
+    let notif2open = true;
+
+    const notif1 = new window.Notification(
+        "Emergency",
+        {
+            body: "There's an emergency going on",
+            tag: "bob",
+        },
+    );
+    notif1.onclose = () => {
+        console.log("notif 1 was closed");
+        notif1open = false;
+    };
+
+    const notif2 = new window.Notification(
+        "Emergency",
+        {
+            body: "It's still going on",
+            tag: "bob",
+        },
+    );
+    notif2.onclose = () => {
+        console.log("notif 1 was closed");
+        notif1open = false;
+    };
+
+    setTimeout(() => {
+        if (notif1open) {
+            console.log("notif 1 is still open");
+        }
+        if (notif2open) {
+            console.log("notif 2 is still open");
+            notif2.close();
+        }
+    }, 3000);
+}
+
+window.makeitcrash = makeitcrash;
